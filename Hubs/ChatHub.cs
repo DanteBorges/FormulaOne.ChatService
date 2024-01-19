91e6cef20668c35dee824628caebe882fd747fd1
DanielBorges <danielnorges@gmail.com>
2024-01-19 11:05:58 -0300

    first commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,39 @@
+using FormulaOne.ChatService.DataService;
+using FormulaOne.ChatService.Models;
+using Microsoft.AspNetCore.SignalR;
+namespace FormulaOne.ChatService.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        private readonly SharedDb _shared;
+
+        public ChatHub(SharedDb shared) => _shared = shared;
+
+        public async Task JoinChat(UserConnection conn)
+        {
+            await Clients.All
+                .SendAsync(method: "ReceiveMessage", arg1: "admin", arg2: $"{conn.Username} has joined {conn.ChatRoom}");
+
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection conn)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName: conn.ChatRoom);
+
+            _shared.connections[Context.ConnectionId] = conn;
+
+            await Clients.Group(conn.ChatRoom)//ClientProxy
+                .SendAsync(method: "ReceiveMessage", arg1: "admin", arg2: $"{conn.Username} has joined {conn.ChatRoom}");
+        }
+        public async Task SendMessage(string msg)
+        {
+            if (_shared.connections.TryGetValue(Context.ConnectionId, out UserConnection conn))
+            {
+                await Clients.Group(conn.ChatRoom)
+                .SendAsync(method: "ReceiveSpecificMessage", arg1: conn.Username, arg2: msg);
+            };
+        }
+
+    }
+}
